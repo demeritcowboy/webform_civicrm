fba8d680e6ffbae54890d185f22b1516f5126b0d
demeritcowboy <demeritcowboy@hotmail.com>
2023-01-23 13:21:19 -0500

    do temporary hack in iats master

--- tests/src/FunctionalJavascript/ContributionIatsTest.php
+++ tests/src/FunctionalJavascript/ContributionIatsTest.php
@@ -20,9 +20,19 @@ final class ContributionIatsTest extends WebformCivicrmTestBase {
     parent::setUp();
 
     // Download installs and enables!
+    /*
     $result = civicrm_api3('Extension', 'download', [
       'key' => "com.iatspayments.civicrm",
     ]);
+     */
+    // @todo Make downloaded extensions more like carrot and have the extensions downloaded once in the yml file before the whole test run to a known location.
+    system('cd ' . escapeshellarg(\CRM_Core_Config::singleton()->extensionsDir) . '; git clone https://github.com/iATSPayments/com.iatspayments.civicrm.git > /dev/null 2> /dev/null');
+    $f = rtrim(\CRM_Core_Config::singleton()->extensionsDir, '/') . '/com.iatspayments.civicrm/CRM/Iats/iATSServiceRequest.php';
+    $content = str_replace('current($processresult->AUTHORIZATIONRESULT)', '((array) $processresult->AUTHORIZATIONRESULT)[0] ?? ""', file_get_contents($f));
+    $content = str_replace('current($processresult->TRANSACTIONID)', '((array) $processresult->TRANSACTIONID)[0] ?? ""', $content);
+    file_put_contents($f, $content);
+    $result = civicrm_api3('Extension', 'refresh', ['remote' => FALSE]);
+    $result = civicrm_api3('Extension', 'enable', ['keys' => 'com.iatspayments.civicrm']);
 
     // Legacy CC
     $params = [
