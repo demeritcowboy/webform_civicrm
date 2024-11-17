17d53e5d3d685fa8f19d73162c488e7295a9f577
demeritcowboy <demeritcowboy@hotmail.com>
2024-11-17 09:44:43 -0500

    try different function

--- tests/src/FunctionalJavascript/ExistingContactElementTest.php
+++ tests/src/FunctionalJavascript/ExistingContactElementTest.php
@@ -610,7 +610,8 @@ States. State/Province - New Jersey.
 
     // Place fields for each contact on their own page and enable saving drafts
     $webform =  Webform::load($this->webform->getOriginalId());
-    $elements = Yaml::decode($webform->get('elements'));
+//    $elements = Yaml::decode($webform->get('elements'));
+    $elements = $webform->getElementsDecodedAndFlattened();
     $this->htmlOutput('id is ' . $this->webform->getOriginalId() . ' raw elements is ' . var_export($webform->get('elements'), true) . ' elements is ' . var_export($elements, true));
     $elements_new = [
       'page1' => ['#type' => 'webform_wizard_page', '#title' => 'Page 1', 'civicrm_1_contact_1_fieldset_fieldset' => $elements["civicrm_1_contact_1_fieldset_fieldset"]],
